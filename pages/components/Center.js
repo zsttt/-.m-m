8faa953c2216a0730e7e4861fd81f0791f1c1677
zsttt <zstap17@gmail.com>
2020-12-30 19:54:23 -0500

    main

---
+++ pages/components/Center.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { useState } from "react";
+
+const Center = () => {
+    return (
+        <Div>
+
+        </Div>
+    )
+};
+
+export default Center;
--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,15 @@
+import { useState, useEffect } from "react"; 
 import  Game from '../pages/components/Game.js'
+import Center from '../pages/components/Center.js'
 
 const Home = () => {
+
   return (
-    <div className = "flex flex-col flex-center justify-center h-screen bg-red-500">
+    <Center>
       <div className = "flex flex-col text-center items-center bg-blue-200">
         Hello <Game />
       </div>     
-    </div>
+    </Center>
   )
 }
 
